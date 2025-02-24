9e8b764a05b3800d19cfaec4115e4f4cac5eafbf
patrycjaprzybysz <100605325+patrycjaprzybysz@users.noreply.github.com>
2025-02-24 21:19:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+---
+
+### 📜 **index.js** (szablon kodu dla studentów)
+```js
+/**
+ * Funkcja analizuje tablicę liczb i zwraca obiekt z informacjami:
+ * - sumą liczb
+ * - liczbą elementów parzystych i nieparzystych
+ * - najmniejszą i największą liczbą
+ * @param {number[]} numbers - Tablica liczb
+ * @returns {object|null} - Obiekt z analizą lub null dla pustej tablicy
+ */
+function analyzeNumbers(numbers) {
+    // ✏️ Tutaj napisz swoją implementację
+    
+}
+
+// Eksportujemy funkcję, aby testy mogły ją wykorzystać
+module.exports = analyzeNumbers;
+
